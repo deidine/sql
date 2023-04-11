85c53dd3684b4f79fb08cf3a2451a4f2f3349787
deidine/cheigeur <92623952+deidine@users.noreply.github.com>
2023-04-11 21:10:15 +0000

    Update user privillage.sql

--- user privillage.sql
+++ user privillage.sql
@@ -4,6 +4,6 @@
  FLUSH PRIVILEGES;
  DROP USER 'deidine'@'localhost';
  select user from MySQl.user;--show  users
- 
+ mysql -u root -p    --create shell with mysql -u user name -p passord
 
 --mysqli_connect("192.168.43.24","deidine","deidine","hospital");
